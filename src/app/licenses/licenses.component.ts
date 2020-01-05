a716ebb85a1b448524f8ee125c5a9423fe7884fb
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-01-05 14:13:22 +0300

    Update certificates page

--- src/app/licenses/licenses.component.ts
+++ src/app/licenses/licenses.component.ts
@@ -13,12 +13,20 @@ import { LicensePopupComponent } from './license-popup/license-popup.component';
 export class LicensesComponent {
   public licenses: License[] = [
     {
-      title: 'Аттестат соответствия',
+      title: 'Свидетельство о государственной регистрации',
       imgSrc: 'assets/img/certificate-1.jpg'
     },
     {
-      title: 'Аттестат соответствия',
+      title: 'Сертификат соответствия',
       imgSrc: 'assets/img/certificate-2.jpg'
+    },
+    {
+      title: 'Сертификат качества (eng)',
+      imgSrc: 'assets/img/certificate-3.jpg'
+    },
+    {
+      title: 'Сертификат качества (rus)',
+      imgSrc: 'assets/img/certificate-4.jpg'
     }
   ];
 
